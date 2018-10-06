7e43148e8aa28a85165e822af34c367b22d586a6
MrPoudel <baalkissna2012@gmail.com>
2018-10-06 18:32:12 -0400

    - Updated main.c.

--- Examples/main.c
+++ Examples/main.c
@@ -7,7 +7,9 @@ int main()
   int i;
   printf("Enter the number:\n");
   scanf("%d",&i);
+  //Function to calculate square of a number
   f_square(i);
+  //Function to calculate cube of a number
   f_cube(i);
   return 0;
 }
