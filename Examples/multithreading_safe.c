b76e9976406ba6c31de6fcf06eddcf061fb2d42b
Bal <baalkissna2012@gmail.com>
2018-10-26 14:48:37 -0400

    multithreading

--- Examples/multithreading_safe.c
+++ Examples/multithreading_safe.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdbool.h>
+#include <signal.h>
 
+bool RUN = 1;
+
+
+static void stopHandler(int sign) {
+    printf("received ctrl-c");
+    RUN = false;
+}
 
 /* This is our thread function.  It is like main(), but for a thread */
 void *threadFunc(void *arg)
 {
+	signal(SIGINT, stopHandler);
+	signal(SIGTERM, stopHandler);
+
+	//RUNNING =false;
 	char *str;
 	int i = 0;
 
 	str=(char*)arg;
 
-	while(i < 10 )
+	while(RUN)
 	{
 		usleep(1);
 		printf("threadFunc says: %s\n",str);
 		++i;
 	}
 
+	usleep(5000);
+
 	return NULL;
 }
+
 
 int main(void)
 {
@@ -29,14 +45,18 @@ int main(void)
 	pthread_create(&pth,NULL,threadFunc,"processing...");
 
 	/* wait for our thread to finish before continuing */
-	pthread_join(pth, NULL /* void ** return value could go here */);
+	
 
-	while(i < 10 )
+	while(RUN)
 	{
 		usleep(1);
 		printf("main() is running...\n");
 		++i;
 	}
+
+	printf("waiting thread");
+
+	pthread_join(pth, NULL /* void ** return value could go here */);
 
 	return 0;
 }
