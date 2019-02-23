4f8da729c22a6e04747b2a4fedde10b810ea930e
MrPoudel <baalkissna2012@gmail.com>
2019-02-23 18:01:30 +0100

    Update read_number_from_file.c

--- examples/file_RW/read_number_from_file.c
+++ examples/file_RW/read_number_from_file.c
@@ -22,3 +22,32 @@ int main(void)
 
 	return 0;
 }
+
+/*Calculation of Average*/
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	int N, number;
+	double sum =0, average;
+	FILE *ifile;
+	
+	ifile = fopen("studentGrades.txt", "r"); /* open file for reading
+	the "r" stands for reading*/
+	
+	fscanf(ifile, "%d", &N);
+	
+	for (int i = 0; i < N; ++i)
+	{
+		fscanf(ifile, "%d", &number);
+		sum += number;
+	}
+	average = sum/N;
+	printf("%0.2lf\n", average);
+	
+	fclose(ifile);
+	
+	return 0;
+}
+*/
