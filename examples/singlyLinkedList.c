6242c54e3a51bb713240b3eb4ee6c9cb7b9e8d42
Bal Poudel <baalkissna2012@gmail.com>
2019-09-04 14:02:59 -0700

    Uncrustified and checking git-precommit hook

--- examples/singlyLinkedList.c
+++ examples/singlyLinkedList.c
@@ -3,60 +3,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
-	int data;
-	struct Node *next_node;
+struct Node
+{
+   int         data;
+   struct Node *next_node;
 };
 
 typedef struct Node node;
-typedef struct{
-	node *start_node;
+typedef struct
+{
+   node *start_node;
 } linked_list;
 
-void insert(int num, linked_list *list){
-	if(list->start_node == NULL){
-		list->start_node = (node*) malloc(sizeof(node));
-		list->start_node->data = num;
-		list->start_node->next_node = NULL;
-		printf("This is first node");
-		
-		
-	}
-	else{
-		node *n = list->start_node;
-		while(n->next_node != NULL){
-			n = n->next_node;
-		}
-		node *new_node = (node*) malloc(sizeof(node));
-		new_node->data = num;
-		new_node->next_node = NULL;
-		n->next_node = new_node;
-		printf("This is next node");
-	}
-}
-void print_linked_list(linked_list ll){
-	int i;
-	node* n;
-	n = ll.start_node; //ll.start_node;
-	while(n != NULL){
-		printf("%d\n", n->data);
-		n->next_node;
-	}
-}
 
-void main(){
-	int i;
-	linked_list ll;
-    ll.start_node = NULL;
-
-    for (i = 0; i < 10; i++)
-    {
-    	insert(i, &ll);
-    }
-
-    print_linked_list(ll);
+void insert(int num, linked_list *list)
+{
+   if (list->start_node == NULL)
+   {
+      list->start_node            = (node *)malloc(sizeof(node));
+      list->start_node->data      = num;
+      list->start_node->next_node = NULL;
+      printf("This is first node");
+   }
+   else
+   {
+      node *n = list->start_node;
+      while (n->next_node != NULL)
+      {
+         n = n->next_node;
+      }
+      node *new_node = (node *)malloc(sizeof(node));
+      new_node->data      = num;
+      new_node->next_node = NULL;
+      n->next_node        = new_node;
+      printf("This is next node");
+   }
 }
 
 
+void print_linked_list(linked_list ll)
+{
+   int  i;
+   node *n;
 
+   n = ll.start_node;      //ll.start_node;
+   while (n != NULL)
+   {
+      printf("%d\n", n->data);
+      n->next_node;
+   }
+}
+
+
+void main()
+{
+   int         i;
+   linked_list ll;
+
+   ll.start_node = NULL;
+
+   for (i = 0; i < 10; i++)
+   {
+      insert(i, &ll);
+   }
+
+   print_linked_list(ll);
+}
 
